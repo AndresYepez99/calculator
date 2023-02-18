7c06a60a32a88f82cbb42eebaec763d6d1073b0d
Andres Yepez <andresyepezmn@gmail.com>
2023-02-18 13:12:00 -0500

    Completar secuencia de comando para operaciones y historial de operaciones

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const body = document.querySelector('body');
 //alternar tema de claro/oscuro 
 const toolbar = document.querySelector('.toolbar');
@@ -18,15 +20,143 @@ btnDark.addEventListener(('click'), () => {
 //animacion para botones al hacer click
 const buttons = document.querySelectorAll('button');
 const arrButtons = [...buttons];
+
 arrButtons.forEach(element => {
     element.addEventListener(('click'), () => {
-        element.classList.add('hover-button')
+        element.classList.add('hover-button');
     });
 
-});
-
-arrButtons.forEach(element => {
     element.addEventListener(('transitionend'), (e) => {
         element.classList.remove('hover-button')
     });
+
 });
+
+
+//--------------screen
+const resultScreen = document.querySelector('.result');
+const equal = document.querySelector('#igual');
+
+const bntNumbers = document.querySelectorAll('.btn-numbers');
+const arrBtnNumbers = [...bntNumbers];
+
+const btnOperators = document.querySelectorAll('.btn-operator');
+const arrBtnOperators = [...btnOperators];
+
+let previousValue = '';
+let currrentValue = '';
+let operator = ''; 
+
+
+function updateScreen(element) {
+    if (resultScreen.textContent === '0') {
+        resultScreen.textContent = '';
+        resultScreen.textContent += element.textContent;
+    } else {
+        resultScreen.textContent += element.textContent;
+    }
+}
+
+arrButtons.forEach(element => {
+    element.addEventListener(('click'), () => {
+        updateScreen(element);
+        
+    })
+});
+
+arrBtnNumbers.forEach(element => {
+    element.addEventListener(('click'), () => {
+        currrentValue += element.textContent;
+    })
+});
+
+arrBtnOperators.forEach(element => {
+    element.addEventListener(('click'), () => {
+        previousValue = currrentValue;
+        operator = element.textContent 
+        currrentValue = '';
+        console.log(previousValue)
+        console.log(currrentValue)
+        console.log(operator)
+    })
+});
+
+//limpiar pantalla
+const btnClear = document.querySelector('.btn-delete');
+btnClear.addEventListener(('click'), () => {
+    resultScreen.textContent = '0';
+    previousValue = '';
+    currrentValue = '';
+})
+
+function add(a, b) {
+    console.log('la suma es: ', a+b)
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        resultScreen.textContent = (a + b);
+        currrentValue = a + b;
+    } else if (operator === '-') {
+        resultScreen.textContent = (a - b);
+        currrentValue = a + b;
+    } else if (operator === '*') {
+        resultScreen.textContent = (a * b);
+        currrentValue = a + b;
+    } else if (operator === '/') {
+        resultScreen.textContent = (a / b);
+        currrentValue = a + b;
+    }
+}
+
+const screenHistory = document.querySelector('.operator');
+
+let arrHistory = [];
+let resultEnd;
+
+function resultHistory(element) {
+    arrHistory.push(element + resultEnd);
+    screenHistory.innerHTML = arrHistory
+    arrHistory.forEach(e => {
+        console.log(e);
+    });
+}
+
+equal.addEventListener(('click'), () => {  
+    let value1 = Number(previousValue);
+    let value2 = Number(currrentValue);
+    let result = `${previousValue} + ${currrentValue} = `;
+    operate(operator, value1, value2);
+    resultEnd = resultScreen.textContent + '<br>';
+    resultHistory(result);
+    
+})
+
+
+/*  guardo primer valor
+    guardo segundo valor
+    guado operador
+    llamar funcion para realizar operacion*/
+
+
+
+
+
+
+
+
+
+
+//-----------funciones operaciones
